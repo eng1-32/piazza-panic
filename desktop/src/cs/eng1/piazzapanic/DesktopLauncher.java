77451d7cbf77baa5f383ff01c26ab3df116ca53d
Alistair Foggin <alistair.foggin02@gmail.com>
2022-11-24 16:46:46 +0000

    Initial Commit: Generate libGDX game

---
+++ desktop/src/cs/eng1/piazzapanic/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package cs.eng1.piazzapanic;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import cs.eng1.piazzapanic.PiazzaPanicGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Piazza Panic");
+		new Lwjgl3Application(new PiazzaPanicGame(), config);
+	}
+}
