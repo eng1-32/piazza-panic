80d22a61fe7612b694b0abd882243abd00cac406
Matt Fitzpatrick <m.fitzpatrick285@gmail.com>
2022-12-03 23:24:33 +0000

    Added constructors to all classes in the stations package and added functionality to the CookingStation class

--- core/src/cs/eng1/piazzapanic/stations/ChoppingStation.java
+++ core/src/cs/eng1/piazzapanic/stations/ChoppingStation.java
@@ -1,5 +1,12 @@
 package cs.eng1.piazzapanic.stations;
 
-public class ChoppingStation extends Station {
+import com.badlogic.gdx.graphics.Texture;
+import cs.eng1.piazzapanic.Ingredient;
 
+public class ChoppingStation extends Station {
+    protected Ingredient[] validIngredients;
+    public ChoppingStation(Texture image, float[] position, Ingredient[] Ingredients){
+        super(image, position);
+        validIngredients = Ingredients; //A list of the ingredients that can be used by this station.
+    }
 }
