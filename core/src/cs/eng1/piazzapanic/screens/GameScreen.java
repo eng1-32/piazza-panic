560b37a13bbc727991a5f8942b6993ac945724bf
Alistair Foggin <alistair.foggin02@gmail.com>
2022-12-08 16:31:06 +0000

    Test actor and stages

--- core/src/cs/eng1/piazzapanic/screens/GameScreen.java
+++ core/src/cs/eng1/piazzapanic/screens/GameScreen.java
@@ -1,15 +1,51 @@
 package cs.eng1.piazzapanic.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import cs.eng1.piazzapanic.GameMap;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import cs.eng1.piazzapanic.PiazzaPanicGame;
 
 public class GameScreen implements Screen {
-  final PiazzaPanicGame game;
-  GameMap map;
+
+  private final PiazzaPanicGame game;
+  private final Stage stage;
+
+  static class TestActor extends Actor {
+
+    Texture image;
+    float speed = 25.0f;
+    public TestActor() {
+      image = new Texture(Gdx.files.internal("badlogic.jpg"));
+    }
+
+    @Override
+    public void act(float delta) {
+      super.act(delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+      batch.draw(image, getX(), getY(), getWidth(), getHeight());
+    }
+  }
 
   public GameScreen(PiazzaPanicGame game) {
     this.game = game;
+    OrthographicCamera camera = new OrthographicCamera();
+    ScreenViewport viewport = new ScreenViewport(camera);
+    this.stage = new Stage(viewport);
+    TestActor testActor = new TestActor();
+    testActor.setBounds(5, 5, 200, 200);
+    testActor.setColor(1, 0, 0, 1);
+    testActor.addAction(Actions.moveTo(1000, 50, 5.0f));
+    this.stage.addActor(testActor);
   }
 
   @Override
@@ -19,12 +55,14 @@ public class GameScreen implements Screen {
 
   @Override
   public void render(float delta) {
-
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    stage.act(delta);
+    stage.draw();
   }
 
   @Override
   public void resize(int width, int height) {
-
+    this.stage.getViewport().update(width, height, true);
   }
 
   @Override
@@ -44,6 +82,6 @@ public class GameScreen implements Screen {
 
   @Override
   public void dispose() {
-
+    stage.dispose();
   }
 }
