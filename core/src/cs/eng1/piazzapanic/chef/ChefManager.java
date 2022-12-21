83ef3edf328d012f8308a09bb3cdb1f728bfc744
Alistair Foggin <alistair.foggin02@gmail.com>
2022-12-21 17:38:39 +0000

    Allow selection and deselection of Chef

--- core/src/cs/eng1/piazzapanic/chef/ChefManager.java
+++ core/src/cs/eng1/piazzapanic/chef/ChefManager.java
@@ -3,13 +3,17 @@ package cs.eng1.piazzapanic.chef;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChefManager {
   private final ArrayList<Chef> chefs;
+  private Chef currentChef = null;
 
   public ChefManager(float chefScale) {
     String[] chefSprites = new String[]{
@@ -26,17 +30,40 @@ public class ChefManager {
       chef.setInputEnabled(false);
       chefs.add(chef);
     }
-
-    chefs.get(0).setInputEnabled(true);
   }
 
   public List<Chef> getChefs() {
     return chefs;
   }
 
-  public void addChefsToStage(Stage stage) {
+  public void addChefsToStage(final Stage stage) {
     for (Chef chef : chefs) {
       stage.addActor(chef);
+    }
+    final ChefManager manager = this;
+    stage.addListener(new ClickListener() {
+      @Override
+      public void clicked(InputEvent event, float x, float y) {
+        Actor actorHit = stage.hit(x, y, false);
+        if (actorHit instanceof Chef)
+          manager.setCurrentChef((Chef) actorHit);
+        else
+          manager.setCurrentChef(null);
+      }
+    });
+  }
+
+  public void setCurrentChef(Chef chef) {
+    if (chef == null && currentChef != null) {
+      currentChef.setInputEnabled(false);
+      currentChef = null;
+    }
+    if (currentChef != chef) {
+      if (currentChef != null) {
+        currentChef.setInputEnabled(false);
+      }
+      currentChef = chef;
+      currentChef.setInputEnabled(true);
     }
   }
 }
