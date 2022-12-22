ef5b1afad7f62d127cbf7b1c7ccb9181bb2f0975
Alistair Foggin <alistair.foggin02@gmail.com>
2022-12-22 15:22:29 +0000

    Add Javadoc for collision methods

--- core/src/cs/eng1/piazzapanic/chef/Chef.java
+++ core/src/cs/eng1/piazzapanic/chef/Chef.java
@@ -44,6 +44,9 @@ public class Chef extends Actor {
     super.act(delta);
   }
 
+  /**
+   * Set the input vector based on the input keys for movement
+   */
   private void getInput() {
     inputVector.x = 0;
     inputVector.y = 0;
@@ -67,6 +70,12 @@ public class Chef extends Actor {
     setInputVector(x, y);
   }
 
+  /**
+   * Calculate how far the chef should move based on the input vector while avoiding collisions
+   *
+   * @param delta the time that has passed since the last frame
+   * @return the vector representing how far the chef should move
+   */
   private Vector2 calculateMovement(float delta) {
     Vector2 movement = new Vector2(inputVector.x * speed * delta, inputVector.y * speed * delta);
 
@@ -77,6 +86,15 @@ public class Chef extends Actor {
     return movement;
   }
 
+  /**
+   * Check to see if a point lies within a tile in the collision layer, or if the point is in a chef
+   * or station
+   *
+   * @param x the x-coordinate to check for a collision
+   * @param y the y-coordinate to check for a collision
+   * @return the bounding box of the object that the point lies within. It will be null if the point
+   * does not lie in any object
+   */
   private Rectangle getCollisionObjectBoundaries(float x, float y) {
     Actor actorHit = getStage().hit(x, y, false);
     Cell tileHit = chefManager.getCellAtPosition(x, getY());
@@ -91,15 +109,22 @@ public class Chef extends Actor {
     }
   }
 
+  /**
+   * @param xMovement the amount to move the chef along the x-axis before collision
+   * @return the new change in the x-axis that ensures that the chef does not collide with any
+   * objects
+   */
   private float adjustHorizontalMovementForCollision(float xMovement) {
     if (xMovement > 0.0001f) {
+      // Check right side of chef at top, middle and bottom
       float rightBorder = getX() + getWidth() + xMovement;
       Rectangle hitBoundsBottom = getCollisionObjectBoundaries(rightBorder, getY());
       Rectangle hitBoundsMiddle = getCollisionObjectBoundaries(rightBorder,
           getY() + getHeight() / 2f);
       Rectangle hitBoundsTop = getCollisionObjectBoundaries(rightBorder, getY() + getHeight());
-      float adjustment = -getWidth() - collisionSkin - getX();
 
+      // Calculate new change in x relative to the collision object boundaries
+      float adjustment = -getWidth() - collisionSkin - getX();
       if (hitBoundsBottom != null) {
         xMovement = hitBoundsBottom.x + adjustment;
       }
@@ -110,13 +135,15 @@ public class Chef extends Actor {
         xMovement = Math.min(xMovement, hitBoundsTop.x + adjustment);
       }
     } else if (xMovement < -0.0001f) {
+      // Check left side of chef at top, middle and bottom
       float leftBorder = getX() + xMovement;
       Rectangle hitBoundsBottom = getCollisionObjectBoundaries(leftBorder, getY());
       Rectangle hitBoundsMiddle = getCollisionObjectBoundaries(leftBorder,
           getY() + getHeight() / 2f);
       Rectangle hitBoundsTop = getCollisionObjectBoundaries(leftBorder, getY() + getHeight());
-      float adjustment = collisionSkin - getX();
 
+      // Calculate new change in x relative to the collision object boundaries
+      float adjustment = collisionSkin - getX();
       if (hitBoundsBottom != null) {
         xMovement = hitBoundsBottom.x + hitBoundsBottom.width + adjustment;
       }
@@ -130,14 +157,21 @@ public class Chef extends Actor {
     return xMovement;
   }
 
+  /**
+   * @param yMovement the amount to move the chef along the y-axis before collision
+   * @return the new change in the y-axis that ensures that the chef does not collide with any
+   * objects
+   */
   private float adjustVerticalMovementForCollision(float yMovement) {
     if (yMovement > 0.0001f) {
+      // Check top side of chef at left, middle and right
       float topBorder = getY() + getHeight() + yMovement;
       Rectangle hitBoundsLeft = getCollisionObjectBoundaries(getX(), topBorder);
       Rectangle hitBoundsMiddle = getCollisionObjectBoundaries(getX() + getWidth() / 2f, topBorder);
       Rectangle hitBoundsRight = getCollisionObjectBoundaries(getX() + getWidth(), topBorder);
-      float adjustment = -getHeight() - collisionSkin - getY();
 
+      // Calculate new change in y relative to the collision object boundaries
+      float adjustment = -getHeight() - collisionSkin - getY();
       if (hitBoundsLeft != null) {
         yMovement = hitBoundsLeft.y + adjustment;
       }
@@ -148,13 +182,15 @@ public class Chef extends Actor {
         yMovement = Math.min(yMovement, hitBoundsRight.y + adjustment);
       }
     } else if (yMovement < -0.0001f) {
+      // Check bottom side of chef at left, middle and right
       float bottomBorder = getY() + yMovement;
       Rectangle hitBoundsLeft = getCollisionObjectBoundaries(getX(), bottomBorder);
       Rectangle hitBoundsMiddle = getCollisionObjectBoundaries(getX() + getWidth() / 2f,
           bottomBorder);
       Rectangle hitBoundsRight = getCollisionObjectBoundaries(getX() + getWidth(), bottomBorder);
-      float adjustment = collisionSkin - getY();
 
+      // Calculate new change in y relative to the collision object boundaries
+      float adjustment = collisionSkin - getY();
       if (hitBoundsLeft != null) {
         yMovement = hitBoundsLeft.y + hitBoundsLeft.height + adjustment;
       }
@@ -184,6 +220,13 @@ public class Chef extends Actor {
     return ingredientStack.pop();
   }
 
+  /**
+   * Sets the input vector based on x and y, but ensuring that the vector is never greater than a
+   * length of 1
+   *
+   * @param x the x input value
+   * @param y the y input value
+   */
   public void setInputVector(float x, float y) {
     inputVector.x = x;
     inputVector.y = y;
