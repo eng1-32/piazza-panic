0a1a7b0bfe45e9dd84aa9f07a421e3aae2577db4
Alistair Foggin <alistair.foggin02@gmail.com>
2023-01-20 10:43:09 +0000

    Fix minor JavaDoc issue

--- core/src/cs/eng1/piazzapanic/chef/FixedStack.java
+++ core/src/cs/eng1/piazzapanic/chef/FixedStack.java
@@ -3,9 +3,9 @@ package cs.eng1.piazzapanic.chef;
 import java.util.Stack;
 
 /**
- * A wrapper over java's builtin Stack<T\>
+ * A wrapper over java's builtin Stack for type T
  *
- * @param <T> The class that should be used for all of the stack elements
+ * @param <T> The class that should be used for all the stack elements
  */
 public class FixedStack<T> extends Stack<T> {
 
