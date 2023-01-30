db33c04af238ff2d2b56baff809d060d2ac8a3c3
Matt Fitzpatrick <m.fitzpatrick285@gmail.com>
2023-01-30 22:43:28 +0000

    Added JavaDoc for the CookingStation class and fixed a small bug with the UI

--- core/src/cs/eng1/piazzapanic/stations/CookingStation.java
+++ core/src/cs/eng1/piazzapanic/stations/CookingStation.java
@@ -12,6 +12,10 @@ import java.util.List;
 import java.util.Objects;
 
 
+/**
+ * The CookingStation class is a station representing the place in the
+ * kitchen where you cook patties to be used in making burgers.
+ */
 public class CookingStation extends Station {
 
   protected Ingredient[] validIngredients;
@@ -20,13 +24,30 @@ public class CookingStation extends Station {
   protected float totalTimeToCook = 10f;
   private boolean progressVisible = false;
 
-  //TODO: Create doc strings for functions
+  /**
+   * The constructor method for the class
+   *
+   * @param id            The unique identifier of the station
+   * @param image         The rectangular area of the texture
+   * @param uiController  The controller from which we can get show and hide the action
+   *                      buttons belonging to the station
+   * @param alignment     Dictates where the action buttons are shown
+   * @param ingredients   An array of ingredients used to define what ingredients can be
+   *                      cooked
+   */
   public CookingStation(int id, TextureRegion image, StationUIController uiController,
       StationActionUI.ActionAlignment alignment, Ingredient[] ingredients) {
     super(id, image, uiController, alignment);
     validIngredients = ingredients; //A list of the ingredients that can be used by this station.
   }
 
+  /**
+   * Called every frame. Used to update the progress bar and
+   * check if enough time has passed for the ingredient to be
+   * changed to its half cooked or cooked variant
+   *
+   * @param delta Time in seconds since the last frame.
+   */
   @Override
   public void act(float delta) {
     if (inUse) {
@@ -40,13 +61,22 @@ public class CookingStation extends Station {
           currentIngredient.setIsCooked(true);
         }
         uiController.hideProgressBar(this);
-        uiController.showActions(this, getActionTypes());
         progressVisible = false;
+        uiController.showActions(this, getActionTypes());
       }
     }
     super.act(delta);
   }
 
+  /**
+   * Checks the presented ingredient with the list of
+   * valid ingredients to see if it can be cooked
+   *
+   * @param ingredientToCheck The ingredient presented by the
+   *                          chef to be checked if it can be used
+   *                          by the station
+   * @return true if the ingredient is in the validIngredients array; false otherwise
+   */
   private boolean isCorrectIngredient(Ingredient ingredientToCheck) {
     if (!ingredientToCheck.getIsCooked()) {
       for (Ingredient item : this.validIngredients) {
@@ -58,6 +88,12 @@ public class CookingStation extends Station {
     return false;
   }
 
+  /**
+   * Obtains the actions that can be currently performed depending on
+   * the states of the station itself and the selected chef
+   *
+   * @return actionTypes - the list of actions the station can currently perform.
+   */
   @Override
   public List<StationAction.ActionType> getActionTypes() {
     LinkedList<StationAction.ActionType> actionTypes = new LinkedList<>();
@@ -83,6 +119,12 @@ public class CookingStation extends Station {
     return actionTypes;
   }
 
+  /**
+   * Given an action, the station should attempt to do that action based on the chef that is nearby
+   * or the state of the ingredient currently on the station.
+   *
+   * @param action the action that needs to be done by this station if it can.
+   */
   @Override
   public void doStationAction(StationAction.ActionType action) {
     switch (action) {
@@ -108,7 +150,6 @@ public class CookingStation extends Station {
           if (this.isCorrectIngredient(nearbyChef.getStack().peek())) {
             currentIngredient = nearbyChef.placeIngredient();
           }
-          //TODO: Display a warning when an incorrect ingredient is presented
         }
         uiController.showActions(this, getActionTypes());
         break;
@@ -124,6 +165,13 @@ public class CookingStation extends Station {
     }
   }
 
+  /**
+   * Displays ingredients that have been placed on the station
+   *
+   * @param batch       Used to display a 2D texture
+   * @param parentAlpha The parent alpha, to be multiplied with this actor's alpha, allowing the parent's alpha to affect all
+   *           children.
+   */
   @Override
   public void draw(Batch batch, float parentAlpha) {
     super.draw(batch, parentAlpha);
