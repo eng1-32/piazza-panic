4f3d24e44efe96e08311f2f1b14896302de792d5
Matt Fitzpatrick <m.fitzpatrick285@gmail.com>
2023-01-30 21:57:00 +0000

    Added JavaDoc for the Recipe class

--- core/src/cs/eng1/piazzapanic/food/recipes/Recipe.java
+++ core/src/cs/eng1/piazzapanic/food/recipes/Recipe.java
@@ -6,11 +6,22 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The Recipe class is the parent class of the food classes that
+ * dictates what ingredients are needed to make them
+ */
 public class Recipe {
 
   private final FoodTextureManager textureManager;
   private final String type;
 
+  /**
+   * The constructor method for the class
+   *
+   * @param type            The food object that inherits the class
+   * @param textureManager  The controller from which we can get information on what texture
+   *                        each food object should have
+   */
   public Recipe(String type, FoodTextureManager textureManager) {
     this.type = type;
     this.textureManager = textureManager;
@@ -24,6 +35,11 @@ public class Recipe {
     return type;
   }
 
+  /**
+   * Gets what ingredients are needed to make the food item
+   *
+   * @return The list of ingredients that are needed
+   */
   public List<String> getRecipeIngredients() {
     LinkedList<String> ingredientTypes = new LinkedList<>();
     switch (getType()) {
