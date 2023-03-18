4673053ca5bf79436f6def58386cf307a9e06fac
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-03-18 05:48:32 +0000

    Update and rename Burger.java to Humborge.java

---
+++ core/src/cs/eng1/piazzapanic/food/recipes/Humborge.java
@@ -0,0 +1,10 @@
+package cs.eng1.piazzapanic.food.recipes;
+
+import cs.eng1.piazzapanic.food.FoodTextureManager;
+
+public class Humborge extends Recipe {
+
+  public Humborge(FoodTextureManager textureManager) {
+    super("humborge", textureManager);
+  }
+}
